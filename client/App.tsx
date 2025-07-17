322a07b45466ef7c4985583da4cf13369398eb9a
Harsh260105 <harshpatel17079@gmail.com>
2025-07-17 23:40:49 +0530

    added vercel analytics

--- client/App.tsx
+++ client/App.tsx
@@ -7,7 +7,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-
+import { Analytics } from '@vercel/analytics/react';
 
 const App = () => (
     <TooltipProvider>
@@ -17,6 +17,7 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+      <Analytics />
     </TooltipProvider>
 );
 
